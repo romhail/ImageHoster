854b62d518763992031b3d47cea2eb9ef482f0e7
romhail <romhail@gmail.com>
2020-12-18 09:22:59 +0530

    fetching records based on id
    Changes to fix issue 1 : fetching records based on id.

--- src/main/java/ImageHoster/service/ImageService.java
+++ src/main/java/ImageHoster/service/ImageService.java
@@ -30,6 +30,11 @@ public class ImageService {
     public Image getImageByTitle(String title) {
         return imageRepository.getImageByTitle(title);
     }
+    
+  //The method calls the getImageByTitle() method in the Repository and passes the title of the image to be fetched
+    public Image getImageById(int id) {
+        return imageRepository.getImage(id);
+    }
 
     //The method calls the getImage() method in the Repository and passes the id of the image to be fetched
     public Image getImage(Integer imageId) {
