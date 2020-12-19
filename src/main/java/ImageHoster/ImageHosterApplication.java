3ddd0d2b4a9cd241c587d1c49c6f3bd221f5ca02
romhail <romhail@gmail.com>
2020-12-19 16:17:06 +0530

    Password fix Partial Changes
    Password check completed. But more code changes are needed.

--- src/main/java/ImageHoster/ImageHosterApplication.java
+++ src/main/java/ImageHoster/ImageHosterApplication.java
@@ -5,10 +5,18 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * ImageHosterApplication class
+ * @author Romil
+ */
 @ComponentScan(value={"ImageHoster.controller", "ImageHoster.config","ImageHoster.service","ImageHoster.repository"})
 @SpringBootApplication
 @EnableAutoConfiguration
 public class ImageHosterApplication {
+	/**
+	 * 
+	 * @param args
+	 */
     public static void main(String[] args) {
         SpringApplication.run(ImageHosterApplication.class, args);
     }
--- src/main/java/ImageHoster/service/UserService.java
+++ src/main/java/ImageHoster/service/UserService.java
@@ -8,28 +8,49 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    //Call the registerUser() method in the UserRepository class to persist the user record in the database
-    public void registerUser(User newUser) {
-        userRepository.registerUser(newUser);
-    }
+	// Call the registerUser() method in the UserRepository class to persist the
+	// user record in the database
+	public boolean registerUser(User newUser) {
+		if (!checkPassword(newUser.getPassword()))
+			return false;
+		userRepository.registerUser(newUser);
+		return true;
+	}
 
-    //Since we did not have any user in the database, therefore the user with username 'upgrad' and password 'password' was hard-coded
-    //This method returned true if the username was 'upgrad' and password is 'password'
-    //But now let us change the implementation of this method
-    //This method receives the User type object
-    //Calls the checkUser() method in the Repository passing the username and password which checks the username and password in the database
-    //The Repository returns User type object if user with entered username and password exists in the database
-    //Else returns null
-    public User login(User user) {
-        User existingUser = userRepository.checkUser(user.getUsername(), user.getPassword());
-        if (existingUser != null) {
-            return existingUser;
-        } else {
-            return null;
-        }
-    }
+	// Since we did not have any user in the database, therefore the user with
+	// username 'upgrad' and password 'password' was hard-coded
+	// This method returned true if the username was 'upgrad' and password is
+	// 'password'
+	// But now let us change the implementation of this method
+	// This method receives the User type object
+	// Calls the checkUser() method in the Repository passing the username and
+	// password which checks the username and password in the database
+	// The Repository returns User type object if user with entered username and
+	// password exists in the database
+	// Else returns null
+	public User login(User user) {
+		User existingUser = userRepository.checkUser(user.getUsername(), user.getPassword());
+		if (existingUser != null) {
+			return existingUser;
+		} else {
+			return null;
+		}
+	}
+
+	private boolean checkPassword(String password) {
+		if (password.matches(".*[a-zA-Z]+.*")) {
+			if (password.matches(".*[0-1]+.*")) {
+				String str = password.replaceAll("[a-zA-Z]", "");
+				str = str.replaceAll("[0-9]", "");
+				if (!str.equalsIgnoreCase("")) {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
 
 }
